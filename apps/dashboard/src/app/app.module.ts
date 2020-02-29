3089197bff868a0b1cbce47ff8eed807171ffa38
Sinan Caliskan <42204545+codersinan@users.noreply.github.com>
2020-02-29 14:47:09 +0300

    add material lib for @angular/material

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { EffectsModule } from '@ngrx/effects';
+// import { AppEffects } from './app.effects';
+// import { StoreRouterConnectingModule } from '@ngrx/router-store';
+// import { StoreModule } from '@ngrx/store';
+// import { reducers, metaReducers } from './reducers';
+// import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+// import { environment } from '../environments/environment';
 
+import { MaterialModule } from "@app/material";
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
-    BrowserAnimationsModule
+    MaterialModule
+    // EffectsModule.forRoot([AppEffects]),
+    // StoreRouterConnectingModule.forRoot(),
+    // StoreModule.forRoot(reducers, {
+    //   metaReducers,
+    //   runtimeChecks: {
+    //     strictStateImmutability: true,
+    //     strictActionImmutability: true
+    //   }
+    // }),
+    // StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
